d9789831583d836f630a259328b61669cce18ebf
Luthfiahmad12 <luthfiafif625@gmail.com>
2025-03-22 00:49:39 +0700

    add: user authenticate

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Passenger extends Model
@@ -14,9 +16,15 @@ class Passenger extends Model
         'name',
         'email',
         'phone_number',
+        'user_id'
     ];
 
-    public function bookings()
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function bookings(): HasMany
     {
         return $this->hasMany(Booking::class);
     }
